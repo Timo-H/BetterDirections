15e96a98dc15ed066922822d7651a78e143f51f1
Timo Hak <timo.hak@casema.nl>
2021-06-01 16:25:50 +0200

    Keybindings toegevoegd, en een Keyhandler die activeert als er een key ingedrukt wordt, en dan checkt of die met een keybinding overeenkomt

--- src/main/java/com/spacialnightmare/betterdirections/events/ModEvents.java
+++ src/main/java/com/spacialnightmare/betterdirections/events/ModEvents.java
@@ -1,17 +1,29 @@
 package com.spacialnightmare.betterdirections.events;
 
+import com.spacialnightmare.betterdirections.setup.ClientProxy;
+import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.chunk.IChunk;
+import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.event.world.ChunkEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class ModEvents {
 
+    // Activates each time a chunk is loaded (for now it just gets the chunk coords)
     @SubscribeEvent
     public void placeNodes(ChunkEvent.Load event) {
         IChunk chunk = event.getChunk();
         ChunkPos pos = chunk.getPos();
-        System.out.println(pos.getXStart() + ", " + pos.getXEnd() + ", " + pos.getZStart() + ", " + pos.getZEnd() +
-                ", " + pos + ", " + pos.x + ", " + pos.z);
+    }
+
+    // Activates each time a key is pressed and looks if it matches with one of the set keybindings
+    @SubscribeEvent
+    public void Keyhandler(InputEvent.KeyInputEvent event) {
+        KeyBinding[] keyBindings = ClientProxy.keyBindings;
+
+        if (keyBindings[0].isPressed()) {
+            System.out.println("Key binding ="+keyBindings[0].getKeyDescription());
+        }
     }
 }
--- src/main/java/com/spacialnightmare/betterdirections/setup/ClientProxy.java
+++ src/main/java/com/spacialnightmare/betterdirections/setup/ClientProxy.java
@@ -1,16 +1,30 @@
 package com.spacialnightmare.betterdirections.setup;
 
 import com.spacialnightmare.betterdirections.BetterDirections;
+import com.spacialnightmare.betterdirections.util.Registration;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = BetterDirections.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ClientProxy implements IProxy{
 
+    public static KeyBinding[] keyBindings;
+
     @Override
     public void init() {
+        // Make keybindings for easy use of the mod
+        keyBindings = new KeyBinding[1];
+
+        keyBindings[0] = new KeyBinding("key.set_waypoint.desc", 66, "key.mod_keybinds.category");
+
+        // Register the keybindings
+        for (int i = 0; i < keyBindings.length; ++i) {
+            ClientRegistry.registerKeyBinding(keyBindings[i]);
+        }
     }
 
     @Override
